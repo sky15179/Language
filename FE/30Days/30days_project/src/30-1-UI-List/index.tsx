b5f72839d86567a771f65dd1ad9752213e5c7e08
王智刚 <wangzhigang03@maoyan.com>
2019-11-26 19:43:07 +0800

    列表

--- FE/30Days/30days_project/src/30-1-UI-List/index.tsx
+++ FE/30Days/30days_project/src/30-1-UI-List/index.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import logo from '.././logo.svg'
-import './style.less'
+// import './style.less'
+import './index.css';
 
 export default class List extends Component {
     constructor(props: any) {
@@ -20,13 +21,21 @@ export default class List extends Component {
     }
 
     render() {
+      const datas = [
+        {
+          title: '数据一',
+          subtitle: '标题一',
+          img: 'https://raw.githubusercontent.com/sky15179/Images/master/20190916170919.png'
+        }
+      ]
         return (
-          <div>
+          <div style={ { flex: 1} }>
             测试
             <ul className="item-title">
               111
             </ul>
             <ul>111</ul>
+            <ListItem title="测试2" subtitle="测试副标题" ></ListItem>
           </div>
         )
     }
@@ -46,27 +55,63 @@ interface ListItemProps {
     subtitle?: string
 }
 
+interface ButtonProps {
+  title: string
+  onClick: () => void
+}
+
+class Button extends Component<ButtonProps> {
+  onFocus = false
+
+  render () {
+        return (
+          <div
+            onClick={event => {
+              this.props.onClick()
+            }}
+            onFocus={e => {
+              this.onFocus = true
+            }}
+            onBlur={e => {
+              this.onFocus = false
+            }}
+            className={this.onFocus ? 'itemButtonFocus' : 'itemButton'}>
+            {this.props.title}
+          </div>
+        )
+  }
+}
+
 class ListItem extends Component<ListItemProps> {
   createIcon = (icon: string) => {
-    return <img src={logo} className="App-logo" alt="logo" />
+    return (
+        <img src={logo} className="itemContainer-image" alt="logo" />
+    )
   }
 
   createTitle = (title: string) => {
-    return <ul>title</ul>
+    return <div className="itemContainer-title">{title}</div>
   }
 
   createSubTitle = (title: string) => {
-    return <p>title</p>
+    return <div className="itemContainer-subtitle">{title}</div>
   }
 
   render() {
     let { icon, title, subtitle } = this.props
     return (
-      <div>
-        {this.createIcon('123')}
-        {!!title ? this.createTitle(title) : null}
+      <div className="itemContainer">
+        <div>{this.createIcon('123')}</div>
+        <div className="itemTitleContainer">
+          {!!title ? this.createTitle(title) : null}
+          {!!subtitle ? this.createSubTitle(subtitle) : null}
+        </div>
+        <Button
+          title="添加"
+          onClick={() => {
+            alert('点击')
+          }}></Button>
       </div>
     )
   }
 }
-
