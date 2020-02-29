b9d35f863d6c4d7172caf932de01dcf461af71d1
王智刚 <sky15179@126.com>
2020-02-29 12:53:25 +0800

    init swiftui

---
+++ Swift/SwiftUI/swiftUI/swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI
+//
+//  Created by 王智刚 on 2020/2/26.
+//  Copyright © 2020 王智刚. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
